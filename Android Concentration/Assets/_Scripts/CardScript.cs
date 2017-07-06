0807e7ed0757736075bcf4ed8f2288506a01815f
ACG2000 <adam.co.grant@gmail.com>
2017-07-06 10:48:14 -0700

    added Cards and background, and some scripts

---
+++ Android Concentration/Assets/_Scripts/CardScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardScript : MonoBehaviour {
+
+    // Use this for initialization
+    GameObject card;
+    Rigidbody cardRB;
+	void Start () {
+        cardRB= GetComponent<Rigidbody>();
+    }
+	
+    public void flip () {
+        cardRB.MoveRotation(new Quaternion(0, 180, 0, 180));
+    }
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
---
+++ Android Concentration/Assets/_Scripts/GameController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    // Use this for initialization
+    bool match;
+    int cards;
+    int cardsUp;
+    int gridX = 6;
+    int gridY = 9;
+    float vSpacing = 1F;
+    float hSpacing = 1;
+    GameObject[] blocks;
+        
+    void Start()
+    {
+        blocks = GameObject.FindGameObjectsWithTag("Card");
+        for (float y = 0; y < gridY; y++)
+        {
+            for (float x = 0; x < gridX; x++)
+            {
+                Instantiate(blocks[Random.Range(0, blocks.Length)], transform.position = new Vector3(x * vSpacing - 2.5F, y * hSpacing - 4F, 0), Quaternion.identity);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
